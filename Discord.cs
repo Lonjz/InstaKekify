d70e9b17ec3eb3fb88514fc84d683d7189214686
lon <lonwastaken@users.noreply.github.com>
2021-03-26 16:42:33 -0400

    e
    e

---
+++ Discord.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscordRPC;
+using DiscordRPC.Logging;
+
+namespace ig_keker
+{
+    public class Discord
+    {
+        public string status;
+        private DiscordRpcClient client = new DiscordRpcClient("825083228367880213");
+        public Discord()
+        {
+            status = "Idling...";
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            client.Initialize();
+        }
+
+        public Discord(string e)
+        {
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            client.Initialize();
+            status = e;
+        }
+        public void start()
+        {
+            client.SetPresence(new DiscordRPC.RichPresence()
+
+            {
+                State = "Status: " + status,
+                Details = "InstaKekify",
+                Timestamps = Timestamps.Now,
+                Assets = new Assets() 
+                { 
+                    LargeImageKey = "logo",
+                    LargeImageText = "Coded by: Lon#0666"
+                }
+
+            });
+
+        }
+        public void changeState(string e)
+        {
+
+            status = e;
+
+            client.SetPresence(new DiscordRPC.RichPresence()
+
+            {
+                State = "Status: " + status,
+                Details = "InstaKekify",
+                Timestamps = Timestamps.Now,
+                Assets = new Assets()
+                {
+                    LargeImageKey = "logo",
+                    LargeImageText = "Coded by: Lon#0666"
+                }
+
+            });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InstaSharper;
+using InstaSharper.API;
+using InstaSharper.Classes;
+
+namespace ig_keker
+{
+    class Program
+    {
+
+        #region Hidden
+        private string username;
+        private string password;
+        private static UserSessionData user;
+        private static IInstaApi api;
+        private static Discord rpc = new Discord();
+        private static bool running = true;
+        #endregion
+
+        static void Main(string[] args)
+        {
+
+            rpc.start();
+
+            while (running)
+            {
+                showMenu();
+                string choiceS = Console.ReadLine();
+                manageChoice(choiceS);
+
+            }
+
+            user = new UserSessionData();
+
+        }
+
+        static void manageChoice(string choiceS)
+        {
+            if (choiceS.Equals("1"))
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                return;
+            }
+
+            else if (choiceS.Equals("2"))
+            {
+                ChangeStatus();
+
+                return;
+            }
+
+            else if (choiceS.Equals("3"))
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Exiting...");
+
+                running = false;
+
+                return;
+            }
+
+            else return;
+
+        }
+
+        static void showMenu()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\n \n");
+            Console.WriteLine("Welcome to InstaKekify! Enter a number to select your module...");
+            Console.WriteLine("==================================");
+            Console.WriteLine("[1] - Login");
+            Console.WriteLine("[2] - Change Discord Status");
+            Console.WriteLine("[3] - Exit");
+            Console.WriteLine("==================================");
+            Console.WriteLine("\n \n");
+        }
+
+        static void ChangeStatus()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Enter a Discord Status for the RPC:");
+
+            string customstatus = Console.ReadLine();
+            
+            rpc.changeState(customstatus);
+            
+            Console.WriteLine("Set DiscordRPC status to " + customstatus + "! enter any key to continue...");
+            Console.ReadLine();
+        }
+
+    }
+
+}
